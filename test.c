50b876ab350b7f28a544554866a2a87a7689cfd2
DJ-BOB <33143533+DJ-BOB@users.noreply.github.com>
2017-10-27 16:01:44 +1100

    Create test.c

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int
+main(int argc, char *argv[]) {
+	char songs[7][30]= {{"Jesus Walks"},{"Spacehip"},{"Get em high"},
+ {"All Falls down"},{"Two Words"},{"Last Call"},{"Family Business"}} ;
+ 
+ printf("%s", songs[3]);
+ return 0;
